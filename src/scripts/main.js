089c8606e9d43d1f240fd9016e313c662a4656b6
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2025-01-06 18:49:00 +0200

    create landing page KateVR

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+import { videoPlayer } from './utils/videoPlayer';
+import { burgerMenu } from './utils/burgerMenu';
+import { languageSelector } from './utils/languageSelector';
+import { languageNavMenu } from './utils/languageNavMenu';
+import {
+  aboutUsSliderConfig,
+  headerSliderConfig,
+  slider,
+} from './utils/slider';
+
+document.addEventListener('DOMContentLoaded', () => {
+  videoPlayer();
+  burgerMenu();
+  languageSelector();
+  languageNavMenu();
+  slider(aboutUsSliderConfig);
+  slider(headerSliderConfig);
+});
