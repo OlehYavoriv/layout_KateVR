c949208d02728bb700f7f323a2585f37a10a0f92
Oleh Yavoriv <oleh.yavoriv@gmail.com>
2025-01-12 19:21:50 +0200

    create help and faq modals and add full size modal window with order-payment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,21 @@ import { videoPlayer } from './utils/videoPlayer';
 import { burgerMenu } from './utils/burgerMenu';
 import { languageSelector } from './utils/languageSelector';
 import { languageNavMenu } from './utils/languageNavMenu';
+import { modalTemplate } from './utils/modals';
+import { faqAccordion } from './utils/faq-accordion';
+import { fullSizeModal } from './utils/fullSizeModal';
 import {
   aboutUsSliderConfig,
   headerSliderConfig,
   slider,
 } from './utils/slider';
+
+const HELP_LINK_SELECTOR = 'a[href="#help"]';
+const HELP_MODAL_SELECTOR = '#modal-help';
+const FAQ_LINK_SELECTOR = 'a[href="#faq"]';
+const FAQ_MODAL_SELECTOR = '#modal-faq';
+const ORDER_MODAL_SELECTOR = '.order-modal';
+const ORDER_LINK_SELECTOR = 'a[href="#buy-now"]';
 
 document.addEventListener('DOMContentLoaded', () => {
   videoPlayer();
@@ -17,4 +27,9 @@ document.addEventListener('DOMContentLoaded', () => {
   languageNavMenu();
   slider(aboutUsSliderConfig);
   slider(headerSliderConfig);
+  modalTemplate(HELP_LINK_SELECTOR, HELP_MODAL_SELECTOR);
+  modalTemplate(FAQ_LINK_SELECTOR, FAQ_MODAL_SELECTOR);
+  modalTemplate(ORDER_LINK_SELECTOR, ORDER_MODAL_SELECTOR);
+  faqAccordion();
+  fullSizeModal();
 });
